91ee6431679a06faa2350781fa029b19fdf7c436
kui66de <37660690+kui66de@users.noreply.github.com>
2019-10-17 15:15:18 +0100

    front-end

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Medilab Free Bootstrap HTML5 Template</title>
+  <title>Book a Doctor to you</title>
   <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
   <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
 
@@ -12,6 +12,7 @@
   <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="icon" type="image/png" href="img/icon.png">
 </head>
 
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
@@ -27,15 +28,17 @@
 				        <span class="icon-bar"></span>
 				        <span class="icon-bar"></span>
 				      </button>
-              <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+              <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 120px; margin-top: -28px;"></a>
             </div>
             <div class="collapse navbar-collapse navbar-right" id="myNavbar">
               <ul class="nav navbar-nav">
                 <li class="active"><a href="#banner">Home</a></li>
                 <li class=""><a href="#service">Services</a></li>
                 <li class=""><a href="#about">About</a></li>
-                <li class=""><a href="#testimonial">Testimonial</a></li>
                 <li class=""><a href="#contact">Contact</a></li>
+                <li class=""><a href="#help">Help</a></li>
+                <li class=""><a href="#login">Log in</a></li>
+                <li class=""><a href="#register">Sign up</a></li>
               </ul>
             </div>
           </div>
@@ -44,13 +47,14 @@
       <div class="container">
         <div class="row">
           <div class="banner-info">
-            <div class="banner-logo text-center">
+<!--            <div class="banner-logo text-center">
               <img src="img/logo.png" class="img-responsive">
-            </div>
+            </div>-->
             <div class="banner-text text-center">
-              <h1 class="white">Healthcare at your desk!!</h1>
+                <h1 class="drbook" color="white">DR.Book</h1>
+              <h1 class="white">Easy, convenient, online doctor services.</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <br>tempor incididunt ut labore et dolore magna aliqua.</p>
-              <a href="#contact" class="btn btn-appoint">Make an Appointment.</a>
+              <a href="#contact" class="btn btn-appoint">Make an Appointment</a>
             </div>
             <div class="overlay-detail text-center">
               <a href="#service"><i class="fa fa-angle-down"></i></a>

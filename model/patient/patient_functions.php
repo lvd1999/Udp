77b008a3ab76e9119f85362b8d5393dd4b3a2078
Lee <patricklee0221@gmail.com>
2019-11-16 23:54:48 +0000

    patient profile now display county

--- model/patient/patient_functions.php
+++ model/patient/patient_functions.php
@@ -45,13 +45,23 @@ function get_patient($patient_pps) {
 
 function get_address($patient_pps) {
     global $db;
-    $query = 'SELECT a.id, a.street_address, a.town_city, a.county_id, a.postcode, c.name AS county_name FROM ((addresses as a INNER JOIN patients as p ON p.address_id = a.id)INNER JOIN counties as c) WHERE pps_num = :patient_pps';
+    $query = 'SELECT a.id, a.street_address, a.town_city, a.county_id, a.postcode, c.name AS county_name FROM ((addresses as a INNER JOIN patients as p ON p.address_id = a.id)INNER JOIN counties as c ON a.county_id = c.id) WHERE pps_num = :patient_pps';
     $statement = $db->prepare($query);
     $statement->bindValue(":patient_pps", $patient_pps);
     $statement->execute();
     $record_list = $statement->fetch();
     $statement->closeCursor();
     return $record_list;
+}
+
+function get_counties() {
+    global $db;
+    $query = 'SELECT * FROM counties;';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $counties = $statement->fetchAll();
+    $statement->closeCursor();
+    return $counties;
 }
 
 //function update_address($patient_pps, $firstname, $lastname, $birthdate, $gender, $contact, $email) {

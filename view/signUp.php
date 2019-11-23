8baadbab38a5b7f5cfd7bd1a6367dd33f8dea0cf
callMeDrStrange <47150136+callMeDrStrange@users.noreply.github.com>
2019-11-23 19:58:55 +0000

    sign up form changed

--- view/signUp.php
+++ view/signUp.php
@@ -1,114 +1,149 @@
-<title>Sign Up</title>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+        <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+        <title>Sign Up</title>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+        <link href="../Content/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../Content/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../Content/css/style.css" rel="stylesheet" type="text/css"/>
+        <style type="text/css">
 
-<?php include 'header.php'; ?>
+            *, *::before, *::after{
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
 
-<style type="text/css">
+                -webkit-transition: all 0.3s ease-in-out;
+                transition: all 0.3s ease-in-out;
+            }
 
-    *, *::before, *::after{
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
+            #footer{
+                position: relative;
+                height: 20%;
+                margin-top: 100px;
+                width: 100%;
+            }
+            .Dsign{
+                margin-left: 200px;
+            }
 
-        -webkit-transition: all 0.3s ease-in-out;
-        transition: all 0.3s ease-in-out;
-    }
+            .PDsign{
+                padding-top: 7em;
+                max-width: 100%;
+                margin: auto;
+                display: block;
+                text-align: center;
+                width: 100%;
+            }
 
-    #footer{
-        position: relative;
-        height: 25%;
-        margin-top: 15em;
-        width: 100%;
-    }
+            figure{
+                width: 220px;
+                height: 230px;
+                overflow: hidden;
+                position: relative;
+                display: inline-block;
+                border: 1px solid gray;
+                border-radius: 50%;
+                box-shadow: 0 0 5px #ddd;
+                margin: 6em;
+            }
 
-    .PDsign{
-        padding-top: 7em;
-        max-width: 100%;
-        margin: auto;
-        display: block;
-        text-align: center;
-        width: 60%;
-    }
+            figcaption{
+                position: absolute;
+                left: 0; right: 0;
+                top: 0; bottom: 0;
+                text-align: center;
+                font-weight: bold;
+                width: 100%;
+                height: 100%;
+                display: table;
+            }
 
-    figure{
-        width: 220px;
-        height: 230px;
-        overflow: hidden;
-        position: relative;
-        display: inline-block;
-        border: 5px solid #fff;
-        border-radius: 50%;
-        box-shadow: 0 0 5px #ddd;
-        margin: 6em;
-    }
+            figcaption div{
+                display: table-cell;
+                vertical-align: middle;
+                position: relative;
+                top: 20px;
+                opacity: 0;
+                color: #2c3e50;
+                text-transform: uppercase;
+                
+            }
 
-    figcaption{
-        position: absolute;
-        left: 0; right: 0;
-        top: 0; bottom: 0;
-        text-align: center;
-        font-weight: bold;
-        width: 100%;
-        height: 100%;
-        display: table;
-    }
+            figcaption div:after{
+                position: absolute;
+                left: 0; right: 0;
+                bottom: 40%;
+                text-align: center;
+                margin: auto;
+                width: 0%;
+                height: 2px;
+                background: #2c3e50;
+                text-decoration: none;
+            }
 
-    figcaption div{
-        display: table-cell;
-        vertical-align: middle;
-        position: relative;
-        top: 20px;
-        opacity: 0;
-        color: #2c3e50;
-        text-transform: uppercase;
-    }
+            figure:hover figcaption{
+                background: rgba(29, 78, 95, 0.1);
+                
+            }
 
-    figcaption div:after{
-        position: absolute;
-        content: "";
-        left: 0; right: 0;
-        bottom: 40%;
-        text-align: center;
-        margin: auto;
-        width: 0%;
-        height: 2px;
-        background: #2c3e50;
-    }
+            figcaption:hover div{
+                opacity: 1;
+                top: 0;
+                
+            }
 
-    figure img{
-        -webkit-transition: all 0.5s linear;
-        transition: all 0.5s linear;
-        -webkit-transform: scale3d(1, 1, 1);
-        transform: scale3d(1, 1, 1);
-    }
+            figcaption:hover div:after{
+                width: 50%;
+            }
 
-    figure:hover figcaption{
-        background: rgba(255,255,255,0.3);
-    }
+            figure:hover img{
+                -webkit-transform: scale3d(1.02, 1.02, 1);
+                transform: scale3d(1.02, 1.02, 1);
+            }
 
-    figcaption:hover div{
-        opacity: 1;
-        top: 0;
-    }
+        </style>
 
-    figcaption:hover div:after{
-        width: 50%;
-    }
+    </head>
+    <body>
+        <section class="banner">
+            <div >
+                <nav class="navbar navbar-default navbar-fixed-top" style="height:73px; background: #1D4E5F;opacity: 0.9;">
+                    <div class="container">
+                        <div class="col-md-12">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="#"><img src="../Content/img/logo.png" class="img-responsive" style="width: 40px; margin-top: -12px;"></a>
+                                <a id="navlogoname" class="navbar-brand" href="../index.php">Dr.Book</a>
+                            </div>
+                            <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="../index.php">Home</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </section>
+        <section class="PDsign">
+            <figure class="Psign">
+                <img src="../Content/img/man.png" alt="patient"/>
+                <figcaption><div><a href="Patient/patient_signUp.php">Patient</a></div></figcation>
+            </figure>
 
-    figure:hover img{
-        -webkit-transform: scale3d(1.2, 1.2, 1);
-        transform: scale3d(1.2, 1.2, 1);
-    }
-
-</style>
-<section class="PDsign">
-    <figure class="Psign">
-        <img src="../Content/img/user-profile.png" width=220px height=230px/>
-        <figcaption><div><a href="Patient/patient_signUp.php">Patient</a></div></figcation>
-    </figure>
-
-    <figure class="Dsign">
-        <img src="../Content/img/doctor-icon.jpg" width=220px height=230px/></a>
-        <figcaption><div><a href="Doctor/doctor_signUp.php">Doctor</a></div></figcation>
-    </figure>
-</section>
-
-
+            <figure class="Dsign">
+                <img src="../Content/img/doctor_signup.jpg" alt="doctor"/>
+                <figcaption><div><a href="Doctor/doctor_signUp.php">Doctor</a></div></figcation>
+            </figure>
+        </section>
+        <?php include '../view/footer.php'; ?>
+    </body>
+</html>
